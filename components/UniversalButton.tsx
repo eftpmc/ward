ce3abe17b136323011929984071365bec31780cc
SaturnEFTPMC <zackhhi@gmail.com>
2023-11-11 16:00:11 -0500

    button fix

--- components/UniversalButton.tsx
+++ components/UniversalButton.tsx
@@ -1,4 +1,10 @@
-export default function UniversalButton({ text, href, ariaLabel }) {
+interface UniversalButtonProps {
+  text: string;
+  href: string;
+  ariaLabel: string;
+}
+
+export default function UniversalButton({ text, href, ariaLabel }: UniversalButtonProps) {
   return (
     <a
       className="py-2 px-3 flex rounded-md no-underline hover:bg-btn-background-hover border"
